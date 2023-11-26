cdee3eb4bee59c408099cc759807f0d376bf41f9
forz <forz666@yandex.ru>
2023-11-26 16:49:40 +0400

    config dev

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,17 +7,17 @@ import (
 )
 
 type Config struct {
-	AppHost     string `env:"APP_HOST" env-required:"true"`
-	AppPort     string `env:"APP_PORT" env-required:"true"`
-	AppEnv      string `env:"APP_ENV" env-required:"true"`
-	PG_Host     string `env:"PG_HOST" env-required:"true"`
-	PG_Port     string `env:"PG_PORT" env-required:"true"`
-	PG_User     string `env:"PG_USER" env-required:"true"`
-	PG_Password string `env:"PG_PASSWORD" env-required:"true"`
-	PG_DB       string `env:"PG_DB" env-required:"true"`
+	AppHost    string `env:"APP_HOST" env-required:"true"`
+	AppPort    string `env:"APP_PORT" env-required:"true"`
+	AppEnv     string `env:"APP_ENV" env-required:"true"`
+	PgHost     string `env:"PG_HOST" env-required:"true"`
+	PgPort     string `env:"PG_PORT" env-required:"true"`
+	PgUser     string `env:"PG_USER" env-required:"true"`
+	PgPassword string `env:"PG_PASSWORD" env-required:"true"`
+	PgDB       string `env:"PG_DB" env-required:"true"`
 }
 
-func Load() Config {
+func Load() *Config {
 	var cfg Config
 	appErr := cleanenv.ReadConfig(".env", &cfg)
 	if appErr != nil {
@@ -25,6 +25,6 @@ func Load() Config {
 		fmt.Println(errMessage)
 		os.Exit(1)
 	}
-	return cfg
+	return &cfg
 
 }
